1ffa62bd1886c9917a76d1205e61b661ba03ca44
AugustoSPorto <augusto.porto.pro@gmail.com>
2025-11-25 00:00:23 -0300

    final touch - a3

--- Distributed Systems/Assessments/Atividade3/src/main.rs
+++ Distributed Systems/Assessments/Atividade3/src/main.rs
@@ -1,7 +1,7 @@
 use std::sync::{Arc, Condvar, Mutex};
 use std::thread;
-use std::time::Duration;
 
+// Permite que o struct SharedQueue seja clonado
 #[derive(Clone)]
 struct SharedQueue<T> {
     data: Arc<(Mutex<Vec<T>>, Condvar, Condvar)>,
@@ -10,7 +10,7 @@ struct SharedQueue<T> {
 
 impl<T> SharedQueue<T> {
     pub fn new(capacity: usize) -> Self {
-        SharedQueue {
+        Self {
             data: Arc::new((Mutex::new(Vec::new()), Condvar::new(), Condvar::new())),
             capacity,
         }
@@ -18,33 +18,36 @@ impl<T> SharedQueue<T> {
 
     pub fn push(&self, item: T) {
         let (lock, not_empty, not_full) = &*self.data;
+        // Bloqueia o mutex para acessar o buffer
         let mut buffer = lock.lock().unwrap();
-
         while buffer.len() == self.capacity {
+            // Espera até que haja espaço no buffer
             buffer = not_full.wait(buffer).unwrap();
         }
-
+        // Adiciona o item ao buffer
         buffer.push(item);
         not_empty.notify_one();
     }
 
     pub fn pop(&self) -> T {
         let (lock, not_empty, not_full) = &*self.data;
+        // Bloqueia o mutex para acessar o buffer
         let mut buffer = lock.lock().unwrap();
-
         while buffer.is_empty() {
+            // Espera até que haja um item no buffer
             buffer = not_empty.wait(buffer).unwrap();
         }
-
+        // Remove o item do buffer
         let item = buffer.remove(0);
         not_full.notify_one();
-
+        // Retorna o item removido
         item
     }
 }
 
 fn main() {
-    let queue = SharedQueue::new(10);
+    let buffer_capacity = 100;
+    let queue = SharedQueue::new(buffer_capacity);
     let num_producers = 3;
     let num_consumers = 2;
     let items_per_producer = 50;
@@ -61,8 +64,8 @@ fn main() {
             for i in 1..=items_per_producer {
                 println!("|* Producer {p} produced {i} *|");
                 queue_producers.push((p, i));
-                thread::sleep(Duration::from_millis(10));
             }
+
         }));
     }
 
@@ -71,6 +74,7 @@ fn main() {
         let queue_consumers = queue.clone();
 
         list_consumers.push(thread::spawn(move || {
+            
             loop {
                 let (producer, item) = queue_consumers.pop();
                 
@@ -80,8 +84,8 @@ fn main() {
                 }
 
                 println!("-- Consumer {c} consumed {item} from producer {producer}");
-                thread::sleep(Duration::from_millis(50));
             }
+
         }));
     }
 
